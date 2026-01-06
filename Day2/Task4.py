104c7faaa22af8a6993c3915fe25174c9aa36522
Nivedh0 <nived385@gmail.com>
2026-01-06 11:20:55 +0530

    day2 commit

---
+++ Day2/Task4.py
@@ -0,0 +1,17 @@
+attempts=3
+while attempts>0:
+    user=input("Enter Username:")
+    pswd=input("Enter password:")
+
+
+    if user=="admin" and pswd=="123":
+            print("Login Successfull")
+            break
+
+    else:
+            attempts-=1
+            print("invalid")
+             
+if attempts==0:
+    print("No attempts left")
+
