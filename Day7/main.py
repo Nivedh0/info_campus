a6a5aa008f9b0b1440bc61d7374f73e9a3cbfd42
Nivedh0 <nived385@gmail.com>
2026-01-13 16:16:08 +0530

    Day 6 commit

---
+++ Day7/main.py
@@ -0,0 +1,5 @@
+import math_utils
+
+print("Addition:",math_utils.add(1,2))
+print("Substraction:",math_utils.sub(1,2))
+print("Multiplication:",math_utils.mul(1,2))
---
+++ Day7/math_utils.py
@@ -0,0 +1,8 @@
+def add(a,b):
+    return a+b
+
+def sub(c,d):
+    return c-d
+
+def mul(e,f):
+    return e*f
