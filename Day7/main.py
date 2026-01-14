9499fd89ab998547aaf281f58d36bf08f3857f0e
Nivedh0 <nived385@gmail.com>
2026-01-14 16:24:19 +0530

    Delete Day7 directory

--- Day7/main.py
+++
@@ -1,5 +0,0 @@
-import math_utils
-
-print("Addition:",math_utils.add(1,2))
-print("Substraction:",math_utils.sub(1,2))
-print("Multiplication:",math_utils.mul(1,2))
--- Day7/math_utils.py
+++
@@ -1,8 +0,0 @@
-def add(a,b):
-    return a+b
-
-def sub(c,d):
-    return c-d
-
-def mul(e,f):
-    return e*f
